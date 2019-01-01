b8139c7d26ab086c3221c7b733572c2182fc63c3
lgxlyh <lgxlyh@gmail.com>
2019-01-01 21:15:50 +0800

    init

---
+++ js/ajax.js
@@ -0,0 +1,54 @@
+function lgxajax(url){
+var xmlhttp;
+var txt;
+function loadXMLDoc(url)
+{
+xmlhttp=null;
+if (window.XMLHttpRequest)
+  {// code for Firefox, Opera, IE7, etc.
+  xmlhttp=new XMLHttpRequest();
+  }
+else if (window.ActiveXObject)
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+if (xmlhttp!=null)
+  {
+  //xmlhttp.onreadystatechange=state_Change(xmlhttp);
+  xmlhttp.open("GET",url,false);
+  //xmlhttp.open("GET",url,true);
+  //xmlhttp.open("GET",url+"?t=" + Math.random(),false);
+  xmlhttp.send(null);
+return xmlhttp.responseText;
+  //xmlhttp.onreadystatechange=state_Change;
+// if(state_Change==200){
+//return txt;
+//}
+}
+else
+  {
+  alert("Your browser does not support XMLHTTP.");
+  }
+}
+function state_Change()
+{
+if (xmlhttp.readyState==4)
+  {// 4 = "loaded"
+  if (xmlhttp.status==200)
+    {// 200 = "OK"
+    //document.getElementById(id).innerHTML=xmlhttp.responseText;
+	txt=xmlhttp.responseText;
+//alert(txt);
+	return 200;
+    }
+  else
+    {
+    alert("Problem retrieving data:" + xmlhttp.statusText);
+    }
+  }
+
+}
+return loadXMLDoc(url);
+//return txt;
+}
+
